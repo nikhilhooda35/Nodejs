a053d18485380c9bc1130a65385f7821ed17aa03
Nikhil Hooda <nikhilhooda35@gmail.com>
2024-05-10 01:55:01 +0530

    using express

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
+const app = express();
+
+function myHandler(req, res) {
   if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()}: ${req.url} New Request Rec. \n`;
   const myUrl = url.parse(req.url, true);
@@ -26,11 +29,13 @@ const myServer = http.createServer((req, res) => {
       case "/contact":
         res.end("Contact Us page");
         break;
-      default:
+      default: 
         res.end("404 not found");
         break;
     }
   });
-});
+}
+
+const myServer = http.createServer(myHandler);
 
 myServer.listen(8000, () => console.log("Server Started!"));
